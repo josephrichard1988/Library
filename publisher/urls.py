8fb9b2bb256d2ad7b68437d8f0b476446e25c442
Richard <richard.joseph@ves.ac.in>
2023-02-25 14:32:56 +0530

    Publisher

--- publisher/urls.py
+++ publisher/urls.py
@@ -5,4 +5,8 @@ from publisher import views as publisher_view
 urlpatterns = [
     path('create-publisher-page', publisher_view.create_publisher_page_fn,
          name="create_publisher_page"),
+    path('update-publisher-page/<int:publisher_id>', publisher_view.update_publisher_page_fn,
+         name="update_publisher_page"),
+    path('delete-publisher-page/<int:publisher_id>', publisher_view.delete_publisher_page_fn,
+         name="delete_publisher_page"),
 ]
--- publisher/views.py
+++ publisher/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import publishersForm
 from books import models as book_model
 
@@ -17,4 +17,30 @@ def create_publisher_page_fn(request):
         'publisher_form': publisher_form,
         'publisher_table_data': publisher_table_data
     }
-    return render(request, "publishers/create_publisher_page.html", data)
+    return render(request, "publishers/manage_publisher.html", data)
+
+
+def update_publisher_page_fn(request, publisher_id):
+    print(publisher_id)
+    publisher = book_model.publishers.objects.get(id=publisher_id)
+    publisher_form = publishersForm(instance=publisher)
+
+    if request.method == "POST":
+        publisher_form_submit = publishersForm(
+            request.POST, instance=publisher)
+        if publisher_form_submit.is_valid():
+            publisher_form_submit.save()
+            return redirect('create_publisher_page')
+
+    publisher_table_data = book_model.publishers.objects.all()
+    data = {
+        'publisher_form': publisher_form,
+        'publisher_table_data': publisher_table_data
+    }
+    return render(request, "publishers/manage_publisher.html", data)
+
+
+def delete_publisher_page_fn(request, publisher_id):
+    publisher = book_model.publishers.objects.get(id=publisher_id)
+    publisher.delete()
+    return redirect('create_publisher_page')
